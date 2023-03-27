98a6275628f045261ab8834394c092204c7d48be
Your Name <hello@nanmanas.com>
2023-03-27 12:35:12 +0700

    test iphone15

--- test-db.py
+++ test-db.py
@@ -24,7 +24,7 @@ def main():
     cur.execute('CREATE TABLE products (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, price float NOT NULL)')
 
     # make a list of 20 test product names and prices - assuming we have electronics store
-    products = [('Iphone 13', 40000.00), ('Iphone 13 Pro', 42000.00), ('Iphone 13 Pro Max', 45000.00), ("Samsung Galaxy S21", 35000.00), ("Samsung Galaxy S21 Ultra", 40000.00), ("Samsung Galaxy S21 Plus", 37000.00), ("OnePlus 9 Pro", 50000.00), ("OnePlus 9", 45000.00), ("OnePlus 9R", 40000.00), ("OnePlus 9T", 45000.00), ("OnePlus Nord 2", 40000.00), ("OnePlus Nord CE 5G", 35000.00), ("OnePlus Nord N200 5G", 30000.00), ("OnePlus Nord N10 5G", 25000.00), ("OnePlus Nord N100", 20000.00), ("OnePlus Nord N10", 15000.00), ("OnePlus Nord N100", 10000.00), ("OnePlus Nord N10", 5000.00), ("OnePlus Nord N100", 2500.00), ("OnePlus Nord N10", 1000.00)]
+    products = [('Iphone 13', 40000.00), ('Iphone 15 Pro', 42000.00), ('Iphone 15 Pro Max', 45000.00), ("Samsung Galaxy S21", 35000.00), ("Samsung Galaxy S21 Ultra", 40000.00), ("Samsung Galaxy S21 Plus", 37000.00), ("OnePlus 9 Pro", 50000.00), ("OnePlus 9", 45000.00), ("OnePlus 9R", 40000.00), ("OnePlus 9T", 45000.00), ("OnePlus Nord 2", 40000.00), ("OnePlus Nord CE 5G", 35000.00), ("OnePlus Nord N200 5G", 30000.00), ("OnePlus Nord N10 5G", 25000.00), ("OnePlus Nord N100", 20000.00), ("OnePlus Nord N10", 15000.00), ("OnePlus Nord N100", 10000.00), ("OnePlus Nord N10", 5000.00), ("OnePlus Nord N100", 2500.00), ("OnePlus Nord N10", 1000.00)]
 
     # insert a new products
     logging.info('Inserting data...')
